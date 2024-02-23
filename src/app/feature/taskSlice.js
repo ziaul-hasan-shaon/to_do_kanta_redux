db8ac773f144ecb975d1cb95ca095da68ea93fd3
ziaul-hasan-shaon <ziaul@devsnest.net>
2024-02-23 14:44:38 +0600

    redux file added

---
+++ src/app/feature/taskSlice.js
@@ -0,0 +1,12 @@
+// import { createSlice } from "@reduxjs/toolkit"
+
+// const initialValue = {
+//     name: '',
+//     email: '',
+//     designation
+// }
+
+// cosnt taskSlice = createSlice({
+//     name: "tasks",
+//     initialState: 
+// })
